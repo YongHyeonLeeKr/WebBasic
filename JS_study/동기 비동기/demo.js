7b49823649124b3c14c5154641d57ae15ed6c15d
marintelli <lyh.enoch@gmail.com>
2023-03-31 21:41:04 +0900

    동기 비동기 - 이벤트 루프 개념 사용하여 설명

--- JS_study/동기 비동기/demo.js
+++ JS_study/동기 비동기/demo.js
@@ -1,16 +1,30 @@
-import fetch from "node-fetch";
+//import fetch from "node-fetch";
 
+
+const setTimeOutTest = () => {
+    setTimeout(()=>{
+    console.log('456');
+}, 0)
+console.log('123');
+}
+
+
+const myCallback = () => {
+    console.log('myCallBack');
+}
 
 const promiseTest =  function() {
  return new Promise((resolver,reject) => {
     resolver(150);
-     
- });
+ }).then((res) => {
+    console.log(res);
+});
 }
 
 
-const reqNaver = function() {
 
+
+const reqNaver = function() {
   fetch(`https://www.naver.com`)
   .then((res) => {
         console.log(res) 
@@ -18,12 +32,5 @@ const reqNaver = function() {
     }).catch((err)=> {
         console.log(err)
     })
-        
-    
 }
-//undefined
-promiseTest().then((res) => {
-    console.log(res);
-})
 
-reqNaver()
