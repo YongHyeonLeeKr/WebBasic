f9bad7428886813ff3968f83aeffc53850225775
marintelli <lyh.enoch@gmail.com>
2023-04-01 08:10:04 +0900

    콜백헬 예시 | 프로미스 | Axios도 프로미스

--- JS_study/동기 비동기/demo.js
+++ JS_study/동기 비동기/demo.js
@@ -10,15 +10,51 @@ console.log('123');
 
 
 const myCallback = () => {
-    console.log('myCallBack');
+    const xmlHttpRequest = new XMLHttpRequest()
+    xmlHttpRequest.open("get", `http://numbersapi.com/#random?min=1&max=10`)
+    xmlHttpRequest.send()
+    xmlHttpRequest.addEventListener("load", (res) => {
+        console.log(res.target.readyState);
+            const xmlHttpRequest2 = new XMLHttpRequest()
+            xmlHttpRequest2.open("get", `http://numbersapi.com/#random?min=1&max=10`)
+            xmlHttpRequest2.send()
+            xmlHttpRequest2.addEventListener("load", (res) => {
+                console.log(res.target.readyState);
+                const xmlHttpRequest3 = new XMLHttpRequest()
+                xmlHttpRequest3.open("get", `http://numbersapi.com/#random?min=1&max=10`)
+                xmlHttpRequest3.send()
+                xmlHttpRequest3.addEventListener("load", (res) => {
+                console.log(res.target.readyState);
+            }) 
+        })
+    })
 }
 
 const promiseTest =  function() {
  return new Promise((resolver,reject) => {
-    resolver(150);
- }).then((res) => {
-    console.log(res);
-});
+    try {
+         // API 요청
+        const result = "api call success"
+        resolver(result);
+    } catch (error) {
+        // api 요청 실패
+        const result = "api call Failure"
+        reject(result);
+    }
+     }).then((res) => {
+        console.log(res);
+    }).catch((res)=> {
+        console.log(res);
+    });
+}
+
+const test_AxiosIsPromise = () => {
+    axios
+        .get(`http://numbersapi.com/#random?min=1&max=10`)
+        .then((res) => {
+            console.log(res)
+            console.log('Axios도 사실 Promise 였다고 한다.')
+        })
 }
 
 
