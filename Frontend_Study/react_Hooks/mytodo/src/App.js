6341ba8cff63414099f4aa7ecb2bf60a12e3fba3
marintelli <lyh.enoch@gmail.com>
2023-04-04 16:01:05 +0900

    react - fetching Data

--- Frontend_Study/react_Hooks/mytodo/src/App.js
+++ Frontend_Study/react_Hooks/mytodo/src/App.js
@@ -4,8 +4,9 @@ import List from './List.jsx'
 
 
 const App = () => {
-  const [todos, setTodos] = useState(['JS공부하기']);
+  const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState();
+  const [loading, setLoading] = useState(false)
   
   const changeInputData = (e) => {
     setNewTodo(e.target.value) /* 할일 정보를 입력하 때마다 상태값을 가지고 있는 것이 좋다.  */
@@ -14,16 +15,40 @@ const App = () => {
   
   const addTodo = (e) => {
     e.preventDefault();
-    setTodos([...todos, newTodo]) /* totos 에  */
+    setTodos([...todos, {id : Math.floor(Math.random() * 1000000), content : newTodo}]) /* totos 에  */
   }
 
+  const fetchingData = () => {
+    setLoading(true);
+
+    setTimeout(() => {
+     const result =  [
+       { id: 1, content: '구름이랑 산책'},
+       { id: 2, content: '초댕이랑 산책'},
+       { id: 3, content: '사업계획서 작성하기'},
+       { id: 4, content: '펜션예약'},
+      ]
+      setLoading(false);
+      setTodos(result);
+    }, 2000);
+    
+      
+      
+      
+  
+
+  
+     }
+
   useEffect(() => {
-    console.log("useEffect() : 새로운 내용이 렌더링 됬습니다. ")
-  })
+    fetchingData() 
+  }, [])
 
    useEffect(() => {
     console.log("useEffect() : 새로운 todos가 추가돼었습니다 : ", todos)
   }, [todos])
+
+
 return ( 
   <>
     <h1>  TODO 어플리케이션 </h1>
@@ -32,10 +57,12 @@ return (
       <button onClick={addTodo}>할일 추가</button>
     </form>
     
-    <List todos={todos}/>
+    <List todos={todos} loading ={loading}/>
 
   </>
   )
 };
 
+
 export default App;
+
