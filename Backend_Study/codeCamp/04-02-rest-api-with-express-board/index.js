5b8e428ff97307fd25f85be5a8a3a1b525e3a912
uvclyh <lyh@uvc.co.kr>
2023-04-20 12:30:16 +0900

    Bk-codecamp-0401-02-rest api

---
+++ Backend_Study/codeCamp/04-02-rest-api-with-express-board/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get('/boards', (req, res) => {
+
+    // 1. 데이터를 조회 하는 로직 -> DB 에 접속하여 데이터 꺼내오기
+    const result = [
+        {number: 1, writer: "철수", titile: "제목입니다~~", contents: "철수 내용"},
+        {number: 1, writer: "철수", titile: "제목입니다~~", contents: "철수 내용"},
+        {number: 1, writer: "철수", titile: "제목입니다~~", contents: "철수 내용"},
+    ]
+    // 2. 꺼내온 결과 응답 주기
+
+    res.send(result)
+
+})
+
+
+
+app.use(express.json());
+app.post('/boards', (req, res) =>{
+    console.log(req.body)
+    // 1. 데이터를 등록하는 로직 => DB에 접속 => 데이터 저장
+
+    // 2. 저장 결과 응답 주기
+
+    res.send("게시물 등록에 성공하였습니다.")
+
+})
+
+
+app.listen(3000, () => {
+    console.log(`Example app listening on port ${3000}`)
+})
+
+
+app.post('/tokens/phone', (req,res) => {
+
+    res.send("인증완료!")
+})
