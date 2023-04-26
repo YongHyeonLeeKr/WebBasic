d19a59916cb3720f5c67417319b8f10450a6763f
uvclyh <lyh@uvc.co.kr>
2023-04-26 09:28:14 +0900

    01-02-Emotion : JS로 스타일링하기

---
+++ Frontend/codecamp/pages/01-02-emotion/index.js
@@ -0,0 +1,12 @@
+import {Email, EmailInput} from '../../styles/emotion.js'
+
+export default function EmotionPage() {
+    return (
+        <>
+            <Email>이메일</Email>
+            <EmailInput type="text"></EmailInput>
+            <button>클릭하세요</button>
+            <img src='/vercel.svg'/>
+        </>
+    )
+}   
