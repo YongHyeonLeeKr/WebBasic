20cbdda10fc99e3401e3b40c8f1782e84995de08
이용현 Noah <lyh@uvc.co.kr>
2023-06-25 09:39:17 +0900

    fr-codecamp-section08-08-02-map-boards

---
+++ Frontend/codecamp/pages/section08/08-02-map-boards/index.js
@@ -0,0 +1,43 @@
+import { gql, useQuery } from '@apollo/client'
+
+
+const FETCH_BOARDS = gql`
+query {
+  fetchBoards{
+    number
+    writer
+    title
+    contents
+  }
+}
+`
+
+export default function StaticRountingMovedPage() {
+
+
+
+    const { data } = useQuery(FETCH_BOARDS)
+    console.log(data)
+    console.log(data?.fetchBoards)
+
+    const mystyle = {
+        margin: "10px",
+        padding: "0px"
+    }
+    return( 
+        <div>
+            {data?.fetchBoards.map(el => 
+                <div>      
+                    <span style={{margin: "10px", padding: "0px"}}> <input type="checkbox"/>  </span>
+                    <span style={{margin: "10px", padding: "0px"}}> {el.number} </span> 
+                    <span style={{margin: "10px", padding: "0px"}}> {el.title}  </span>
+                    <span style={{margin: "10px", padding: "0px"}}> {el.writer}  </span>
+                </div>
+                 )}
+{/*             <div>  {router.query.address}번 게시글 페이지 이동이 완료됐습니다. </div>
+            <div>  작성자 :  {data?.fetchBoard?.writer}</div>
+            <div>  제목 :  {data?.fetchBoard?.title}</div>
+            <div>  작성자 :  {data?.fetchBoard?.contents}</div> */}
+        </div>
+    )
+}
