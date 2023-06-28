a3d4526d3d8c3c26287f7159fc5c0291cc1c1c9a
uvclyh <lyh@uvc.co.kr>
2023-06-28 17:52:30 +0900

    bk-nodeBooks-4.5 노드에서의 클러스터

--- Backend/nodeBooks/ch4/4.5/cluster.js
+++ Backend/nodeBooks/ch4/4.5/cluster.js
@@ -2,9 +2,22 @@ const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
 
+
+/**
+ *  클러스터 - 기본적으로 싱글스레드인 노드가 CPU 코어를 모두 사용할 수 있게 해주는 모듈
+ * - 포트를 공유하는 노드 프로레스를 여러개 둘 수 있음 
+ * - 요청이 많이 들어왔을 때 병렬로 실행된 서버의 개수만큼 분산 요청됨
+ * - 서버에 무기가 덜 감
+ * - 코어가 8개인 서버가 있을 때: 보통은 코어 하나만 활용
+ * - cluster로 코어 하나당 노드 프로세스 하나를 배정 간으
+ * - 성능이 8배가 되는 것은 아니지만 개선은 됨
+ * - 단점 : 컴퓨터 자원(메모리, 세션 등) 공유 못함
+ * - Redis등 별도 서버로 해결 
+ * 
+ */
 if (cluster.isMaster) {
   console.log(`마스터 프로세스 아이디: ${process.pid}`);
-  // CPU 개수만큼 워커를 생산
+  // CPU 개수만큼 워커(프로세스)를 생산
   for (let i = 0; i < numCPUs; i += 1) {
     cluster.fork();
   }
@@ -12,7 +25,7 @@ if (cluster.isMaster) {
   cluster.on('exit', (worker, code, signal) => {
     console.log(`${worker.process.pid}번 워커가 종료되었습니다.`);
     console.log('code', code, 'signal', signal);
-    cluster.fork();
+    cluster.fork(); 
   });
 } else {
   // 워커들이 포트에서 대기
