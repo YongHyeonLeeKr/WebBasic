0d88a56534c6601cc9c21b6b80b4a4e48740db2a
YongHyeonLeeKr <yonghyeon.lee.kr@gmail.com>
2023-08-12 20:43:53 +0900

    bk-codecamp2-11-01-sign-up 단방향 복호화까지

--- Backend/codeCamp2/section11/11-01-signup/src/apis/users/users.service.ts
+++ Backend/codeCamp2/section11/11-01-signup/src/apis/users/users.service.ts
@@ -2,7 +2,11 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { IUsersServiceCreate, IUsersServiceFindOneByEmail } from './interfaces/users-service.interface';
+import {
+  IUsersServiceCreate,
+  IUsersServiceFindOneByEmail,
+} from './interfaces/users-service.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -15,17 +19,22 @@ export class UsersService {
     return this.usersRepository.findOne({ where: { email } });
   }
 
-  async create({ email, password, name, age }: IUsersServiceCreate): Promise<User> {
+  async create({
+    email,
+    password,
+    name,
+    age,
+  }: IUsersServiceCreate): Promise<User> {
     // 중복가입확인
     const user = await this.findOneByEmail({ email });
     if (user) throw new ConflictException('이미 등록된 이메일 입니다. ');
-
+    // 비밀번호 해싱
+    const hashedPassword = await bcrypt.hash(password, 10);
     return this.usersRepository.save({
       email,
-      password,
+      password: hashedPassword,
       name,
       age,
     });
   }
 }
-
