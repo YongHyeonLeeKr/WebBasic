4e544353f1c6e2bf5e4538abfa2e4980b34ec7b8
noahLee <yonghyeon.lee.kr@gmail.com>
2024-05-07 18:35:26 +0900

    BK-board-crud-모든 게시물을 가져오는 Service 만들기

--- Backend/board-crud/src/boards/boards.controller.ts
+++ Backend/board-crud/src/boards/boards.controller.ts
@@ -14,28 +14,28 @@ import { Board } from './entities/board.entity';
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
+  @Get('/')
   getAllBoards() {
-    return this.boardsService.findAll();
+    return this.boardsService.getAllboards();
   }
 
   // @Get(':id')
   // getBoardById(@Param('id') id: number) {
   //   return this.boardsService.findOne(id);
   // }
-
-  @Post()
-  createBoard(@Body() board: Board) {
-    return this.boardsService.create(board);
-  }
-
+  //
+  // @Post()
+  // createBoard(@Body() board: Board) {
+  //   return this.boardsService.create(board);
+  // }
+  //
   // @Put(':id')
   // updateBoard(@Param('id') id: number, @Body() boardData: Partial<Board>) {
   //   return this.boardsService.update(id, boardData);
   // }
-
-  @Delete(':id')
-  deleteBoard(@Param('id') id: number) {
-    return this.boardsService.remove(id);
-  }
+  //
+  // @Delete(':id')
+  // deleteBoard(@Param('id') id: number) {
+  //   return this.boardsService.remove(id);
+  // }
 }
--- Backend/board-crud/src/boards/boards.service.ts
+++ Backend/board-crud/src/boards/boards.service.ts
@@ -5,29 +5,35 @@ import { Board } from './entities/board.entity';
 
 @Injectable()
 export class BoardsService {
-  constructor(
-    @InjectRepository(Board)
-    private boardsRepository: Repository<Board>,
-  ) {}
+  private boards = [];
+  // constructor(
+  //   @InjectRepository(Board)
+  //   private boardsRepository: Repository<Board>,
+  // ) {}
 
-  async findAll(): Promise<Board[]> {
-    return this.boardsRepository.find();
+
+  getAllboards() {
+    return this.boards;
   }
 
+  // async findAll(): Promise<Board[]> {
+  //   return this.boardsRepository.find();
+  // }
+  //
   // async findOne(id: number): Promise<Board> {
   //   return this.boardsRepository.findOne(id);
   // }
-
-  async create(board: Board): Promise<Board> {
-    return this.boardsRepository.save(board);
-  }
-
+  //
+  // async create(board: Board): Promise<Board> {
+  //   return this.boardsRepository.save(board);
+  // }
+  //
   // async update(id: number, boardData: Partial<Board>): Promise<Board> {
   //   await this.boardsRepository.update(id, boardData);
   //   return this.boardsRepository.findOne(id);
   // }
-
-  async remove(id: number): Promise<void> {
-    await this.boardsRepository.delete(id);
-  }
+  //
+  // async remove(id: number): Promise<void> {
+  //   await this.boardsRepository.delete(id);
+  // }
 }
