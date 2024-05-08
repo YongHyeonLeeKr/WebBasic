6eef6d6afe5cb750eca9d6fe8a5d74e56139850f
noahLee <yonghyeon.lee.kr@gmail.com>
2024-05-08 09:55:32 +0900

    BK-board-crud-게시물 생성(Service)

--- Backend/board-crud/src/boards/boards.service.ts
+++ Backend/board-crud/src/boards/boards.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Board } from './models/board.model';
+import { Board, BoardStatus } from './models/board.model';
+import { v1 as uuid } from 'uuid';
 
+/*
+게시물에 관한 로직을 처리하는 곳 
+ */
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
@@ -13,6 +17,22 @@ export class BoardsService {
 
   getAllboards() {
     return this.boards;
+  }
+
+  createBoard(title: string, description: string) {
+    /*
+    게시물 ID는 유니크한 값을 넣어주어야 하는데,
+    데이터베이스를 사용하지 않으므로 UUID 모듈을 이용해서 유니크한 값을 줌
+     */
+    const board: Board = {
+      id: uuid(),
+      title,
+      description,
+      status: BoardStatus.PUBLIC,
+    };
+
+    this.boards.push(board);
+    return board;
   }
 
   // async findAll(): Promise<Board[]> {
