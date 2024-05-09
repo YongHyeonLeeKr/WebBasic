caaaabf6131b88567b7e32ee02570cf217381d6b
noahLee <yonghyeon.lee.kr@gmail.com>
2024-05-09 18:25:50 +0900

    BK-board-crud: 게시물을 위한 Entity 생성

---
+++ Backend/board-crud/src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,29 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common';
+import { BoardStatus } from '../models/board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PUBLIC, BoardStatus.PRIVATE];
+  transform(value: any, metadata: ArgumentMetadata): any {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(
+        `${value}는 게시글 상태 목록에 포함되어 있지 않습니다. `,
+      );
+    }
+
+    return value;
+  }
+
+  private isStatusValid(status: any): boolean {
+    /*
+    StateOptions 배열에 없는 status라면 index의 값은 -1
+     */
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
