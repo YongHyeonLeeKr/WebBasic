72764d7d5d5cdb249674211522a8aa5beab24a66
noahLee <yonghyeon.lee.kr@gmail.com>
2024-07-11 18:33:19 +0900

    JS-싸이월드

---
+++ JS/prj-cyworld/index.js
@@ -0,0 +1,12 @@
+
+const onClickMenuHome = () =>  {
+    document.getElementById('contentFrame').setAttribute("src", 'home.html')
+}
+
+const onClickJukeBox = () =>  {
+    document.getElementById('contentFrame').setAttribute("src", 'jukebox.html')
+}
+
+const onClickGame = () =>  {
+    document.getElementById('contentFrame').setAttribute("src", 'game.html')
+}
