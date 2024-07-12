39603e5d5ad2b704e1e17deeafaffd968225cb75
noahLee <yonghyeon.lee.kr@gmail.com>
2024-07-12 09:40:59 +0900

    JS-싸이월드 마무리

--- JS/prj-cyworld/index.js
+++ JS/prj-cyworld/index.js
@@ -1,12 +1,25 @@
 
 const onClickMenuHome = () =>  {
     document.getElementById('contentFrame').setAttribute("src", 'home.html')
+    document.getElementById('menuHome').style = "color: black; backround-color: white;"
+    document.getElementById('jukeBox').style = "color: white; background-color: #298eb5;"
+    document.getElementById('game').style = "color: white; background-color: #298eb5;"
 }
 
 const onClickJukeBox = () =>  {
     document.getElementById('contentFrame').setAttribute("src", 'jukebox.html')
+    document.getElementById('jukeBox').style = "color: black; backround-color: white; "
+    document.getElementById('menuHome').style = "color: white; background-color: #298eb5;"
+    document.getElementById('game').style = "color: white; background-color: #298eb5;"
+
+
+    
 }
 
 const onClickGame = () =>  {
     document.getElementById('contentFrame').setAttribute("src", 'game.html')
+    document.getElementById('game').style = "color: black; backround-color: white; "
+    document.getElementById('menuHome').style = "color: white; background-color: #298eb5;"
+    document.getElementById('jukeBox').style = "color: white; background-color: #298eb5;"
+
 }
